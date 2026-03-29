e633c4e4ca6b3df1fd7a4bb069986ec0880f6a1c
Alfonso Cejudo <fonz@happeningsoft.com>
2026-03-29 19:24:54 +0200

    chore: remove unused btn_active and btn_inactive image assets

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,8 +1,6 @@
 export const images = {
   bg: require('../assets/images/bg.png'),
-  btnActive: require('../assets/images/btn_active.png'),
-  btnInactive: require('../assets/images/btn_inactive.png'),
-  btnConvert: require('../assets/images/btn_convert.png'),
+btnConvert: require('../assets/images/btn_convert.png'),
   btnConvertPressed: require('../assets/images/btn_convert_pressed.png'),
   tabBar: require('../assets/images/tab_bar.png'),
 };
